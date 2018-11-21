eeb4531fbbfd54c6a59ad434f628a53d018b9a7e
shavez00@yahoo.com <>
2018-11-20 16:08:03 -0800

    change alt attribute to show restaurant name

--- js/main.js
+++ js/main.js
@@ -148,10 +148,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.name + " Image ";
+  image.alt = restaurant.name + " Restaurant Image ";
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
